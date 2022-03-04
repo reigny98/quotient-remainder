e060817b9f19e25664a92bf58a7255d0a2ee1ba5
reigny98 <reigny2016@gmail.com>
2022-03-03 18:52:11 -1200

    added quotient remainder program

---
+++ quotient remainder/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace quotient_remainder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+                int dividend = 30;
+                int divisor = 7;
+                int remainder;
+                int quotient = Math.DivRem(dividend, divisor, out remainder);
+                Console.WriteLine("Quotient = " + quotient);
+                Console.WriteLine("Remainder = " + remainder);
+            
+        }
+    }
+}
